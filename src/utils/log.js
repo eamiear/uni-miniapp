a659121d6401527445e357f7a48670e62ffead17
eamiear <justcall_me_k@163.com>
2021-02-17 19:11:58 +0800

    feat:修改日记配置

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,28 +1,39 @@
 /*
  * @Author: eamiear
  * @Date: 2021-02-07 15:56:28
- * @Last Modified by:   eamiear
- * @Last Modified time: 2021-02-07 15:56:28
+ * @Last Modified by: eamiear
+ * @Last Modified time: 2021-02-15 20:48:41
  */
-import minilog from 'minilog'
+import Minilog from 'minilog'
 import config from '@/config/defaultSettings'
 const name = require('../../package.json').name || 'onbright-app'
 
 const debug = config.debug
-debug ? minilog.enable() : minilog.disable()
+debug ? Minilog.enable() : Minilog.disable()
+
+const logger = new Minilog('app')
 
 export const log = (msg = '') => {
-  minilog.log(`[${name}]: ${msg}`)
+  logger.log(`[${name}]: `, msg)
 }
 
 export const warn = (msg = '') => {
-  minilog.warn(`[${name}]: ${msg}`)
+  logger.warn(`[${name}]: `, msg)
 }
 
 export const info = (msg = '') => {
-  minilog.info(`[${name}]: ${msg}`)
+  logger.info(`[${name}]: `, msg)
 }
 
 export const error = (msg = '') => {
-  minilog.error(`[${name}]: ${msg}`)
+  logger.error(`[${name}]: `, msg)
+}
+
+console.log.call()
+
+export default {
+  log,
+  warn,
+  info,
+  error
 }
